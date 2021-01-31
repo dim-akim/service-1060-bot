12354c66546e4db0bf60bc2836deb77820937cd8
dim-akim <akimov@1060.ru>
2021-01-31 16:59:50 +0300

    У бота появились команды start, help, keyboard

--- service_bot.py
+++ service_bot.py
@@ -1,2 +1,118 @@
-from telegram import Update
-from telegram.ext import Updater, CallbackContext, CommandHandler, MessageHandler
+import functools
+from telegram import Update, ReplyKeyboardMarkup, KeyboardButton
+from telegram.ext import Updater, CallbackContext, CommandHandler, MessageHandler, Filters
+from settings import BOT_TOKEN, ADMIN_IDS
+from log import get_logger
+
+logger = get_logger(__name__)  # Создаем логгер для обработки событий. Сообщения DEBUG - только в консоль
+
+
+def run_service_bot() -> None:
+    """
+    Запускает бота help_admin_1060_bot
+    В боте реализовано ограничение на использование некоторых команд только для администраторов
+    chat_id администраторов можно задать в файле settings
+    """
+    updater = Updater(token=BOT_TOKEN)
+    dispatcher = updater.dispatcher
+
+    help_handler = CommandHandler('help', do_help)
+    dispatcher.add_handler(help_handler)
+
+    keyboard_handler = CommandHandler('keyboard', answer_with_keyboard)
+    dispatcher.add_handler(keyboard_handler)
+
+    message_handler = MessageHandler(Filters.all, do_echo)
+    dispatcher.add_handler(message_handler)
+
+    updater.start_polling()
+    logger.info('help_admin_1060_bot успешно запустился')
+    updater.idle()
+
+
+def admin_access(command):
+    """
+    Декоратор, который ограничивает доступ к команде только для chat_id, которые перечислены в ADMIN_IDS
+    """
+    @functools.wraps(command)
+    def wrapper(*args, **kwargs):
+        update = args[0]
+
+        if update and hasattr(update, 'message'):
+            chat_id = update.message.chat_id
+            if chat_id in ADMIN_IDS:
+                # print(f'Доступ разрешен для {chat_id=}')
+                return command(*args, **kwargs)
+            else:
+                logger.warning(f'Доступ запрещен для {chat_id=}')
+        else:
+            logger.error('Нет атрибута update.message')
+
+    return wrapper
+
+
+def start(update: Update, context: CallbackContext) -> None:
+    pass
+
+
+def do_echo(update: Update, context: CallbackContext) -> None:
+    """
+    Отправляет в чат эхо: Не знаю, что это, наберите /help
+    """
+    # chat_id = update.message.chat_id
+    # text = update.message.text
+
+    update.message.reply_text(  # аналог context.bot.send_message
+        text='Моя твоя понимай нету\n'
+             'Набери /help, чтобы узнать, что я могу'
+    )
+
+
+def do_help(update: Update, context: CallbackContext) -> None:
+    """
+
+    :param update:
+    :param context:
+    :return: None
+    """
+    chat_id = update.message.chat_id
+    logger.info(f'{chat_id=} обратился за помощью')
+
+    first_name = update.message.from_user.first_name
+    last_name = update.message.from_user.last_name
+    text = [
+        f'Привет, {first_name} {last_name}',
+        'Я пока не могу тебе ничего подсказать, сорян!'
+    ]
+    reply_text = '\n'.join(text)
+    update.message.reply_text(text=reply_text)
+
+
+def answer_with_keyboard(update: Update, context: CallbackContext) -> None:
+    # Для клавиатуры нужна матрица (двумерный список) из текстовых элементов
+    # Каждый текстовый элемент - отдельная кнопка
+
+    button_list = [
+        [
+            'col1',
+            'col2',
+            'col3'
+        ],
+        [
+            'row2 col1',
+            'row2 col2'
+        ]
+    ]
+
+    # Теперь собираем кнопки в клавиатуру - объект ReplyKeyboardMarkup
+    # Для одной кнопки в строке можно использовать одномерный список и метод from_column
+    reply_markup = ReplyKeyboardMarkup(
+        button_list,  # список кнопок
+        resize_keyboard=True,  # сжимает кнопки по вертикали до высоты текста
+        one_time_keyboard=True  # убирает клавиатуру после нажатия на кнопку
+    )
+    update.message.reply_text("A three-column menu", reply_markup=reply_markup)
+
+
+if __name__ == '__main__':
+    run_service_bot()
