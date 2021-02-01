39a093c4f9f92072e4766fb90bfcc20a09f046c0
dim-akim <akimov@1060.ru>
2021-02-01 22:50:10 +0300

    Добавил ConversationHandler для диалога о замене картриджа. ДОРАБОТАТЬ

--- service_bot.py
+++ service_bot.py
@@ -1,10 +1,14 @@
 import functools
 from telegram import Update, ReplyKeyboardMarkup, KeyboardButton
-from telegram.ext import Updater, CallbackContext, CommandHandler, MessageHandler, Filters
+from telegram.ext import Updater, CallbackContext, CommandHandler, MessageHandler, Filters, ConversationHandler
 from settings import BOT_TOKEN, ADMIN_IDS
 from log import get_logger
+from gsheets_connector import Printers
 
 logger = get_logger(__name__)  # Создаем логгер для обработки событий. Сообщения DEBUG - только в консоль
+printers = Printers()  # Экземпляр класса Printers, в котором держатся страницы Реестра принтеров
+
+ROOM, DEVICE = range(2)  # Состояния для ConversationHandler
 
 
 def run_service_bot() -> None:
@@ -17,12 +21,20 @@ def run_service_bot() -> None:
     dispatcher = updater.dispatcher
 
     help_handler = CommandHandler('help', do_help)
-    dispatcher.add_handler(help_handler)
-
     keyboard_handler = CommandHandler('keyboard', answer_with_keyboard)
-    dispatcher.add_handler(keyboard_handler)
-
+    conv_handler = ConversationHandler(
+        entry_points=[CommandHandler('change_cartridge', trigger_cartridge_change)],
+        states={
+            ROOM: [MessageHandler(Filters.text, choose_room)],
+            DEVICE: [MessageHandler(Filters.text, choose_device)]
+        },
+        fallbacks=[]
+    )
     message_handler = MessageHandler(Filters.all, do_echo)
+
+    dispatcher.add_handler(help_handler)
+    dispatcher.add_handler(keyboard_handler)
+    dispatcher.add_handler(conv_handler)
     dispatcher.add_handler(message_handler)
 
     updater.start_polling()
@@ -112,6 +124,49 @@ def answer_with_keyboard(update: Update, context: CallbackContext) -> None:
         one_time_keyboard=True  # убирает клавиатуру после нажатия на кнопку
     )
     update.message.reply_text("A three-column menu", reply_markup=reply_markup)
+
+
+def trigger_cartridge_change(update: Update, context: CallbackContext) -> int:
+    keyboard = ['5', '4', '3', '2', '1']
+    reply_markup = ReplyKeyboardMarkup.from_column(
+        keyboard,
+        resize_keyboard=True,
+        one_time_keyboard=True
+    )
+
+    update.message.reply_text('Выберите этаж', reply_markup=reply_markup)
+
+    return ROOM
+
+
+def choose_room(update: Update, context: CallbackContext) -> int:
+    floor_number = update.message.text
+    rooms = [room for room in printers.registry if room[0] == floor_number]
+
+    reply_markup = ReplyKeyboardMarkup.from_column(
+        rooms,
+        resize_keyboard=True,
+        one_time_keyboard=True
+    )
+
+    update.message.reply_text('Выберите кабинет', reply_markup=reply_markup)
+    return DEVICE
+
+
+def choose_device(update: Update, context: CallbackContext) -> int:
+    room_number = update.message.text
+    if len(printers.registry[room_number]) == 1:
+        update.message.reply_text(f'Замена картриджа в принтере {printers.registry[room_number]}')
+    else:
+        keyboard = [printer for printer in printers.registry[room_number]]
+        reply_markup = ReplyKeyboardMarkup.from_row(
+            keyboard,
+            resize_keyboard=True,
+            one_time_keyboard=True
+        )
+
+        update.message.reply_text('Выберите принтер', reply_markup=reply_markup)
+    return ConversationHandler.END
 
 
 if __name__ == '__main__':
