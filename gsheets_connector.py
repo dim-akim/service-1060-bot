cd25bae3ed638a665c3edc08f586c90a8af1534f
dim-akim <akimov@1060.ru>
2021-02-07 15:16:25 +0300

    Добавил метод change_cartidge, который принимает на вход номер кабинета, принтер и дату замены, и проставляет информацию в Реестр

--- gsheets_connector.py
+++ gsheets_connector.py
@@ -7,6 +7,15 @@ client = pygsheets.authorize(service_file='key/python-waldorf-4f0f67808d79.json'
 
 
 class Printers:
+    table: pygsheets.spreadsheet
+    sheets_list: list[pygsheets.worksheet]
+    cartridge_sheet: pygsheets.worksheet
+    summary_sheet: pygsheets.worksheet
+
+    CHANGE_COLUMN = 5  # Столбец с датами замен - Е (5)
+    EVENT_COLUMN = 7  # Столбец с событиями - G (6)
+    START_ROW = 5  # Первый ряд с данными (ряды 1-4 - заголовки)
+
     def __init__(self):
         self.table = client.open_by_key(PRINTERS_GSHEET_KEY)
         self.sheets_list = self.table.worksheets()
@@ -16,7 +25,7 @@ class Printers:
 
         self.registry = self.get_registry()
 
-    def get_registry(self) -> dict:
+    def get_registry(self) -> dict[str, dict[str, pygsheets.worksheet]]:
         registry = {}
         for sheet in self.sheets_list:
             title = sheet.title
@@ -27,6 +36,24 @@ class Printers:
             registry[room][printer] = sheet
         return registry
 
+    def change_cartridge(self, room: str, device: str, date: str) -> tuple[str, str]:
+        """
+        Проставляет дату замены картриджа <date> на страницу принтера <room device>
+        в первую пустую строчку колонки <CHANGE_COLUMN>
+
+        :param room: str, номер кабинета - ключ для соваря <registry>
+        :param device: str, название принтера - ключ для словаря <registry[room]>
+        :param date: str, дата замены в формате ДД.ММ.ГГГГ
+        :return: tuple, предыдущая дата замены и количество месяцев, прошедших с этой даты
+        """
+        printer_sheet = self.registry[room][device]
+        dates = [day for day in printer_sheet.get_col(self.CHANGE_COLUMN) if day]
+        last_row = len(dates)
+        printer_sheet.update_value((last_row + 1, self.CHANGE_COLUMN), date)
+
+        last_date = printer_sheet.cell((last_row, self.CHANGE_COLUMN)).value
+        elapsed = printer_sheet.cell((last_row, self.CHANGE_COLUMN + 1)).value
+        return last_date, elapsed
 
 # Открываем таблицу с помощью семейства методов open_<bla-bla-bla>
 # table = client.open_by_key(PRINTERS_GSHEET_KEY)
@@ -50,6 +77,18 @@ class Printers:
 # sheet.update_value('E6', '30.01.2021')  # Записывает данные, как если бы пользователь вводил руками
 # sheet.update_value('E7', '30.02.2021', parse=True)  # Записывает именно строчку
 
-# printers = Printers()
-# for item in printers.registry:
-#     print(item, printers.registry[item])
+
+if __name__ == '__main__':
+
+    printers = Printers()
+    # for item in printers.registry:
+    #     print(item, printers.registry[item])
+
+    sheet = printers.sheets_list[4]
+    # column = sheet.get_col(printers.CHANGE_COLUMN)
+    # start = printers.START_ROW - 1
+    # for i in range(start, len(column)):
+    #     print(column[i])
+    #     if not column[i]:
+    #         break
+    # print(i + 1)
